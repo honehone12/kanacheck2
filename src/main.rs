2690122dbc30968aaea224031609b4d44a182e28
Twelfthonehone <honehone.41games@gmail.com>
2025-12-02 11:08:49 +0900

    added some dependencies

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Result;
+
+async fn run() -> Result<()> {
+    Ok(())
+}
+
+#[tokio::main]
+async fn main() {
+    if let Err(e) = run().await {
+        panic!("{e}");
+    }
 }
